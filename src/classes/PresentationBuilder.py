e1292687aa3619002bc72e764c7ee8393ed14358
Mustafa Mohammed <mustafa.mohammed0223@gmail.com>
2024-10-06 15:02:26 -0500

    Add local project files

---
+++ src/classes/PresentationBuilder.py
@@ -0,0 +1,7 @@
+class PresentationBuilder:
+    def __init__(self, title, speaker_name_ar, speaker_name_en, text_ar, text_en):
+        self.title = title
+        self.speaker_name_ar = speaker_name_ar
+        self.speaker_name_en = speaker_name_en
+        self.text_ar = text_ar
+        self.text_en = text_en
---
+++ src/main.py
@@ -0,0 +1,3 @@
+if __name__ == "__main__":
+    presentation_builder = PresentationBuilder('test', 'متكلم', 'speaker', 'نص', 'text')
+    
